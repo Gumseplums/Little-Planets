6e90c6e1eeda76714794eae3035c1c363989dc46
Gumseplums <stenmarksgaming@gmail.com>
2025-12-31 20:09:56 +0100

    Working version 0.1.1

--- Little-Planets@Gumseplums/files/Little-Planets@Gumseplums/desklet.js
+++ Little-Planets@Gumseplums/files/Little-Planets@Gumseplums/desklet.js
@@ -1,17 +1,18 @@
 const Desklet = imports.ui.desklet;
 
-function HelloDesklet(metadata, desklet_id) {
+function LittlePlanets(metadata, desklet_id) {
     this._init(metadata, desklet_id);
 }
 
-HelloDesklet.prototype = {
+LittlePlanets.prototype = {
     __proto__: Desklet.Desklet.prototype,
 
     _init: function(metadata, desklet_id) {
         Desklet.Desklet.prototype._init.call(this, metadata, desklet_id);
+
     }
 }
 
 function main(metadata, desklet_id) {
-    return new HelloDesklet(metadata, desklet_id);
+    return new LittlePlanets(metadata, desklet_id);
 }
