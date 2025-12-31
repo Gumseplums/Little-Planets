a584d5b639a2675264b093b26a6f196eacfe093e
Gumseplums <stenmarksgaming@gmail.com>
2025-12-31 20:53:56 +0100

    Add Image attempt one

--- Little-Planets@Gumseplums/files/Little-Planets@Gumseplums/desklet.js
+++ Little-Planets@Gumseplums/files/Little-Planets@Gumseplums/desklet.js
@@ -1,4 +1,7 @@
-const Desklet = imports.ui.desklet;
+const Desklet = imports.ui.desklet;     // Gives me cinnamons desklet things
+const St = imports.gi.St;               // Gives me containers and stuff.
+const GLib = imports.gi.GLib;           // Glib constructs the file path
+const Gio = imports.gi.Gio;             // Gio says which/what file path to follow and then load
 
 function LittlePlanets(metadata, desklet_id) {
     this._init(metadata, desklet_id);
@@ -9,6 +12,12 @@ LittlePlanets.prototype = {
 
     _init: function(metadata, desklet_id) {
         Desklet.Desklet.prototype._init.call(this, metadata, desklet_id);
+
+        this.planet = new St.Icon
+        let grayPlanetFile = Gio.File.new_for_path('images/Bases/Plain/Base-Plain-Gray.png')
+        let grayPlanetIcon = new Gio.FileIcon({ file: grayPlanetFile })
+        this.planet.gicon = grayPlanetIcon
+        this.setContent(this.planet)
 
     }
 }
