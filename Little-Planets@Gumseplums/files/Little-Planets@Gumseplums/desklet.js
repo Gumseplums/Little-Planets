6f72905432c78c11d295c865ac276e351b1d1e52
Gumseplums <stenmarksgaming@gmail.com>
2025-12-31 17:27:43 +0100

    Removed more to make a bare minimum version

--- Little-Planets@Gumseplums/files/Little-Planets@Gumseplums/desklet.js
+++ Little-Planets@Gumseplums/files/Little-Planets@Gumseplums/desklet.js
@@ -1,6 +1,4 @@
 const Desklet = imports.ui.desklet;
-const GLib = imports.gi.GLib;
-const St = imports.gi.St;
 
 const UUID = "Little-Planets@Gumseplums";
 
@@ -10,10 +8,6 @@ function MyDesklet(metadata, desklet_id) {
     this.init(metadata, desklet_id)
 }
 
-function main(metadata, desklet_id) {
-    return new MyDesklet(metadata, desklet_id);
-}
-
 MyDesklet.prototype = {
     _proto_: Desklet.Desklet.prototype,
 
@@ -21,12 +15,9 @@ MyDesklet.prototype = {
 	_init: function(metadata, desklet_id){
 	    Desklet.Desklet.prototype._init.call(this, metadata);
 
-    //do the settings thing
-
-    this.settings = new Settings.DeskletSettings(this, this.metadata["uuid"], desklet_id);
-
-    this.settings.bindPropert(Settings.BindingDirection.IN, "planet-size", "planetSize", this.on_settings_changed);
-    
-
     }
-}    
+}
+
+function main(metadata, desklet_id) {
+    return new MyDesklet(metadata, desklet_id);
+}
